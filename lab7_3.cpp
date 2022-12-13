90894d1f9a8fa5a65b4b35104679b313f92c21ea
poon <poon15112@gmail.com>
2022-12-13 16:53:13 +0700

    lap7_3.cpp comoleted

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
-
+#include <cmath>
 using namespace std;
 
-
+int adiff(int A, int B)
+{
+    int C = abs(A-B)%360;
+    if(C > 180)
+    {
+        C = 360 - C;
+    }
+    return C;
+}
 int main(){
   cout << adiff(180,270);
   cout << adiff(210,45);
@@ -12,4 +20,5 @@ int main(){
   cout << adiff(90,-90);
   cout << adiff(1000,280);
   cout << adiff(60,244);
+  return 0;
 }
